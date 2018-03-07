e39357a9f096122f982e59c681c8a6af12f51621
Vivien Adnot <vivienadnot@amplement.com>
2018-03-07 12:20:49 +0100

    filesystem usage examples with argv and child_process.spawn

---
+++ src/watcher-spawn.js
@@ -0,0 +1,22 @@
+"use strict";
+const fs = require('fs');
+const spawn = require('child_process').spawn;
+const filename = process.argv[2];
+
+if (!filename) {
+
+    throw Error("A file to watch must be specified");
+
+}
+
+fs.watch(filename, () => {
+
+    // ls is a ChildProcess.
+    // We can use its stdin, stdout and stderr Streams to read and write data.
+    let ls = spawn('ls', ['-lh', filename]);
+    // We want to send the standard output from the child process, directly to our own standard output stream.
+    ls.stdout.pipe(process.stdout);
+
+});
+
+console.log(`Now watching ${filename} for changes...`);
---
+++ src/watcher.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+
+const PATH_FILE_WATCHED = 'target.txt';
+
+fs.watch(PATH_FILE_WATCHED, () => console.log('file has changed'));
+
+console.log(`Now watching ${PATH_FILE_WATCHED} for changes...`);
