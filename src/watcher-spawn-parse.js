72d9295f49e95de411772c314fa780b169eada88
Vivien Adnot <vivienadnot@amplement.com>
2018-03-08 17:09:16 +0100

    print events received from the watched file

--- src/watcher-spawn-parse.js
+++ src/watcher-spawn-parse.js
@@ -14,14 +14,22 @@ fs.watch(filename, () => {
     // ls is a ChildProcess.
     // We can use its stdin, stdout and stderr Streams to read and write data.
     let ls = spawn('ls', ['-lh', filename]);
-
     let output = '';
 
-    ls.stdout._destroy('data', (chunk) => output += chunk.toString());
+    // chunk is a Buffer, containing binary data.
+    // it points to a blob of memory allocated by node's native core, outside of the Javascript engine.
+    // it requires decoding to convert to js string.
+    // decoding will copy the buffer's content into node's heap.
+    ls.stdout.on('data', (chunk) => output += chunk.toString());
 
     ls.on('close', () => {
-        let parts = output.split(//);
-    })
+        let parts = output.split(/\s+/);
+        console.dir([
+            parts[0], // permissions
+            parts[4], // size
+            parts[8]  // file name
+        ]);
+    });
 
     // We want to send the standard output from the child process, directly to our own standard output stream.
     ls.stdout.pipe(process.stdout);
