75d24e3f4af788a5fed2c616a4f406250d7806c0
Vivien Adnot <vivienadnot@amplement.com>
2018-03-14 13:45:30 +0100

    added socket listener

---
+++ src/net-watcher.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const fs = require('fs');
+const net = require('net');
+const filename = process.argv[2];
+
+if (!filename) {
+    throw Error('No target filename was specified');
+}
+
+const server = net.createServer((connection) => {
+    // reporting
+    console.log('Subscriber connected');
+    connection.write(`Now watching ${filename} for changes...\n`);
+
+    // watcher setup
+    let watcher = fs.watch(filename, () => {
+        connection.write(`File ${filename} changed: ${Date.now()} \n`);
+    });
+
+    //cleanup
+    connection.on('close', () => {
+        console.log('Subscriber disconnected');
+        watcher.close();
+    });
+});
+
+server.listen(5432, () => {
+    console.log('listening for subscribers');
+});
