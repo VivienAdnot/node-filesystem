a46e7d719d4c2da242552fce42a8639eb711437d
Vivien Adnot <vivienadnot@amplement.com>
2018-03-14 19:14:13 +0100

    implemented socket client

---
+++ src/net-watcher-json-service.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const fs = require('fs');
+const net = require('net');
+const filename = process.argv[2];
+
+if (!filename) {
+    throw Error('No target filename was specified');
+}
+
+const server = net.createServer((connection) => {
+    // reporting
+    console.log('Subscriber connected');
+    //connection.write(`Now watching ${filename} for changes...\n`);
+
+    connection.write(JSON.stringify({
+        type: 'watching',
+        file: filename
+    }) + '\n' );
+
+    // watcher setup
+    let watcher = fs.watch(filename, () => {
+        //connection.write(`File ${filename} changed: ${Date.now()} \n`);
+
+        connection.write(JSON.stringify({
+            type: 'changed',
+            file: filename,
+            timestamp: Date.now()
+        }) + '\n' );
+    });
+
+    //cleanup
+    connection.on('close', () => {
+        console.log('Subscriber disconnected');
+        watcher.close();
+    });
+});
+
+server.listen(5432, () => {
+    console.log('listening for subscribers');
+});
+
+// to connect, open a terminal and run 'telnet localhost 5432'
