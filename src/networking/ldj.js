ef23be3f0cdbc41ce6dd81998ebaf2a0037d0e83
Vivien Adnot <vivienadnot@amplement.com>
2018-03-19 18:19:03 +0100

    wip

--- src/networking/ldj.js
+++ src/networking/ldj.js
@@ -2,7 +2,27 @@ const events = require('events');
 const util = require('util');
 
 //client constructor
-LDJClient = (stream) => {
+const LDJClient = (stream) => {
     events.EventEmitter.call(this);
+
+    let self = this;
+    let buffer = '';
+
+    stream.on('data', (data) => {
+        buffer += data;
+        let boundary = buffer.indexOf('\n');
+
+        while (boundary !== -1) {
+            let input = buffer.substr(0, boundary);
+            buffer = buffer.substr(boundary + 1);
+
+            self.emit('message', JSON.parse(input));
+
+            boundary = buffer.indexOf('\n');
+
+        }
+    })
+
     util.inherits(LDJClient, events.EventEmitter);
 };
+
